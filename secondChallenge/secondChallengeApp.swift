e3912256082f182549a87d42dfebf109f30c9eb7
Bruno Nascimento <bruno.bmn@gmail.com>
2022-04-03 14:17:19 -0300

    Initial Commit

---
+++ secondChallenge/secondChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  secondChallengeApp.swift
+//  secondChallenge
+//
+//  Created by Bruno Nascimento on 03/04/22.
+//
+
+import SwiftUI
+
+@main
+struct secondChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
